9920da984fb59b48df79160cce637a3c0f114c50
efinesbu <efinesbu@gmail.com>
2019-12-01 02:41:06 -0500

    Trends Script

--- TestFile.py
+++ TestFile.py
@@ -2,18 +2,21 @@ import re
 import numpy as np
 import pandas as pd
 
-pop = {'Nevada': {2001: 2.4, 2002: 2.9},
-       'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}
-}
-frame = pd.DataFrame(pop).sort_index(0)
-print(frame)
+# pop = {'Nevada': {2001: 'ABC', 2002: 2.9},
+#        'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}
+# }
+# frame = pd.DataFrame(pop).sort_index(0)
+# print(frame)
+#
+# pdata = {'Ohio': frame['Ohio'][:-1],
+#          'Nevada': frame['Nevada']}
+#
+# print(pd.DataFrame(pdata))
 
-pdata = {'Ohio': frame['Ohio'][:-1],
-         'Nevada': frame['Nevada']}
-
-print(pd.DataFrame(pdata))
-
-
+df = pd.DataFrame({'A': ['bat', 'fooo&#39;ooo', 'bait'],
+                   'B': ['abc', 'bar', 'ertwert&#39;sdfs sdfds&#39;sdf &#39;&#39;&#39;']})
+df = df.replace(to_replace=r'&#39;', value="'", regex=True)
+print(df)
 
 '''
 data = "asdfasdf"
