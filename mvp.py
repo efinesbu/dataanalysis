318db499309fdb01f345595a8d93290a9445955f
efinesbu <efinesbu@gmail.com>
2019-12-15 19:29:37 -0500

    MVP

--- mvp.py
+++ mvp.py
@@ -6,29 +6,32 @@ import deepdish as dd
 #INCREASE DEFAULT COLUMN VISIBILITY
 desired_width = 500
 pd.set_option('display.width', desired_width)
-pd.set_option('display.max_columns', 26)
-pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 25)
+pd.set_option('display.max_rows', 40)
 
 ##################################################################################
 
 imgPath = './Data/emilfine2.jpg'
-img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE) #Load Image in Grayscale
+img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE) # Load Image in Grayscale
 
 # Create data structure
-dict = {'ID': "Img ID",
-          'ImgData': img_gray,
-          'MetaData': {
-            'Text Options': "Say Hi"
+dict = {'ID': 1,
+        'ImgData': img_gray,
+        'MetaData': {
+                'Recommendation': "Say Hi",
+                'User': "User Name"
           }}
 
 # Save data in H5f foromat
 dd.io.save('C:/Users/efine/PycharmProjects/dataanalysis/Data/test2.h5', dict)
 
-
 # Load data
 dictload = dd.io.load('./Data/test2.h5')
 
+a = dictload
+print(a)
+
 # Parse data
-cv2.imshow("Image", dictload['ImgData'])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("Image", dictload['ImgData'])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
