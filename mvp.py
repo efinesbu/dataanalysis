4ec11cc374ff0c1b0b0c9a762424dfb321adc3d9
efinesbu <efinesbu@gmail.com>
2019-12-15 19:20:24 -0500

    MVP

--- mvp.py
+++ mvp.py
@@ -12,20 +12,23 @@ pd.set_option('display.max_rows', 100)
 ##################################################################################
 
 imgPath = './Data/emilfine2.jpg'
-img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
-# exp_img_gray = np.expand_dims(img_gray, axis=2)
+img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE) #Load Image in Grayscale
 
+# Create data structure
 dict = {'ID': "Img ID",
           'ImgData': img_gray,
           'MetaData': {
             'Text Options': "Say Hi"
           }}
 
+# Save data in H5f foromat
 dd.io.save('C:/Users/efine/PycharmProjects/dataanalysis/Data/test2.h5', dict)
 
+
+# Load data
 dictload = dd.io.load('./Data/test2.h5')
 
-
+# Parse data
 cv2.imshow("Image", dictload['ImgData'])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
