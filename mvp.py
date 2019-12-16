d8565ab8ebd94549eef0ee7ce6335732ee06bc80
efinesbu <efinesbu@gmail.com>
2019-12-15 19:47:12 -0500

    MVP

--- mvp.py
+++ mvp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import cv2
 import deepdish as dd
+import numpy as np
 
 ##################################################################################
 #INCREASE DEFAULT COLUMN VISIBILITY
@@ -10,26 +11,37 @@ pd.set_option('display.max_columns', 25)
 pd.set_option('display.max_rows', 40)
 
 ##################################################################################
+#FUNCTIONS
+
+#Add New Recommendation
+def addRec(rec):
+    dict['MetaData']['Recommendations'].append(rec)
+    print(dict['MetaData']['Recommendations'])
+
+
+##################################################################################
 
 imgPath = './Data/emilfine2.jpg'
 img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE) # Load Image in Grayscale
-
+# ex = np.expand_dims(img_gray, axis=2)
 # Create data structure
-dict = {'ID': 1,
-        'ImgData': img_gray,
+dict = {'IDs': [1],
+        'ImgData': [img_gray],
         'MetaData': {
-                'Recommendation': "Say Hi",
-                'User': "User Name"
+                'Recommendations': ["Say Hi"],
+                'Users': ["User Name"]
           }}
 
-# Save data in H5f foromat
-dd.io.save('C:/Users/efine/PycharmProjects/dataanalysis/Data/test2.h5', dict)
+# Add New Recommendation
+addRec(rec = input("Add new recommendation: "))
 
-# Load data
-dictload = dd.io.load('./Data/test2.h5')
 
-a = dictload
-print(a)
+# # Save data in H5f foromat
+# dd.io.save('C:/Users/efine/PycharmProjects/dataanalysis/Data/test2.h5', dict)
+#
+# # Load data
+# dictload = dd.io.load('./Data/test2.h5')
+
 
 # Parse data
 # cv2.imshow("Image", dictload['ImgData'])
