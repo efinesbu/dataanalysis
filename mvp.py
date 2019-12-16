e43cc7dc1403c97201c043ace9dea187a6721ea6
efinesbu <efinesbu@gmail.com>
2019-12-15 21:08:41 -0500

    MVP

--- mvp.py
+++ mvp.py
@@ -30,59 +30,59 @@ def load():
 # ADD IMAGE
 def addImg(dict):
 
-    # imgPath = './Data/emilfine2.jpg'
+    # imgPath = './Data/emilfine2.jpg', './Data/test.jpg'
     imgPath = input("Enter Path: ")
     img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE) # Load Image in Grayscale
     id = len(dict['IDs'])+1
     dict['IDs'].append(id)
     dict['ImgData'].append(img_gray)
-    dict['MetaData']['Recommendations'] = []
-    dict['MetaData']['Users'] = []
-    dict['MetaData']['Label'].append(imgPath)
+    # dict['MetaData']['Recommendations'] = []
+    # dict['MetaData']['Users'] = []
+    dict['Label'].append(imgPath)
     save(dict)
     return dict
 
+def showImg(imgData):
+    cv2.imshow("Image", imgData)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 ##################################################################################
 # MAIN
 
 dict = load()
 
+# print(dict['IDs'])
+# print(dict['MetaData'])
+# print(dict['Label'])
+# print(dict['MetaData'])
+
 # if input("Add new image? y/n: ").lower() == 'y'.strip():
 #     imgData = addImg(dict)
 
-if input("See image options? y/n: ").lower() == 'y'.strip():
-    print(dict['MetaData']['Label'])
-    imagechoice = input("Which image would you like to see?: [enter index, from 0] ")
-    cv2.imshow("image", dict['ImgData'][int(imagechoice)])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# if input("See image options? y/n: ").lower() == 'y'.strip():
+#     print(dict['Label'], '\n', dict['IDs'])
+#     imagechoice = input("Which image would you like to see?: [enter ID #] ")
+#     showImg(dict['ImgData'][int(imagechoice)-1])
 
-
-    # addRec(dict)
+# if input("Would you like to add recommendation? y/n: ").lower() == 'y'.strip():
+#     imagechoice = input("Which image would you like to add a recommendation to?: [enter image ID #] ")
+#     dict['MetaData'][]....
+#     addRec(dict)....
 
 ##################################################################################
-# # # RESET ORIGINAL DATASET
-#
+# # RESET ORIGINAL DATASET
+
 # imgPath = './Data/emilfine2.jpg'
 # img_gray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE) # Load Image in Grayscale
 #
 # dict = {'IDs': [1],
+#         'Label': ["Emil Fine Image"],
 #         'ImgData': [img_gray],
 #         'MetaData': {
-#                 'Recommendations': ["Say Hi"],
-#                 'Users': ["Advisor"],
-#                 'Label': ["Emil Fine Image"]
+#                 'Recommendations': ["Some Text"],
+#                 'Users': ["Advisor ID"]
+#
 #           }}
 # save(dict)
 ##################################################################################
-
-# dictload = load()
-#
-# # Add New Recommendation
-# addRec(dictload)
-
-# Parse data
-# cv2.imshow("Image", dictload['ImgData'])
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
