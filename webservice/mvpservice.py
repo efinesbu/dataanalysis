4221cbc58e5ab845221ff5fb05beedf69577a31d
Emil <efinesbu@gmail.com>
2019-12-21 22:32:54 -0500

    Trend script update

--- webservice/mvpservice.py
+++ webservice/mvpservice.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>Not Much Going On Here</h1>"
-app.run(host='ec2-3-88-217-30.compute-1.amazonaws.com', port=80)
+app.run(host='3.88.217.30', port=80)
