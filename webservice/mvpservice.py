7fc64d99e3fb9f19c33ed83c491672ed3cec03cf
Emil <efinesbu@gmail.com>
2019-12-21 22:51:05 -0500

    commit

--- webservice/mvpservice.py
+++ webservice/mvpservice.py
@@ -2,11 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/ha")
-def hello():
+def hello1():
     return "<h1>Not Much Going On Here, but ha</h1>"
 @app.route("/hi")
-def hello():
+def hello2():
     return "<h1>Not Much Going On Here but hi</h1>"
 
-# app.run(host='3.88.217.30', port=80)
 app.run(host='172.31.90.152', port=80)
+
+# CONNECT IP: http://3.88.217.30/
