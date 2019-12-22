ed39f0973dc782457f5a17d72b94b8b29a423ec8
Emil <efinesbu@gmail.com>
2019-12-21 22:49:21 -0500

    Trend script update

--- webservice/mvpservice.py
+++ webservice/mvpservice.py
@@ -1,8 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/ha")
 def hello():
-    return "<h1>Not Much Going On Here</h1>"
+    return "<h1>Not Much Going On Here, but ha</h1>"
+@app.route("/hi")
+def hello():
+    return "<h1>Not Much Going On Here but hi</h1>"
+
 # app.run(host='3.88.217.30', port=80)
 app.run(host='172.31.90.152', port=80)
